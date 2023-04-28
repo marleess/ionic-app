fba4b28a423ad391e59110a28e668292a73505ff
Marli <marleysumarly@gmail.com>
2023-04-29 05:34:50 +0700

    Setup Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
+import { RouteReuseStrategy, RouterLink, RouterLinkActive } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import {CommonModule} from "@angular/common";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [CommonModule, BrowserModule, IonicModule.forRoot(), IonicModule, AppRoutingModule, RouterLink, RouterLinkActive],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
